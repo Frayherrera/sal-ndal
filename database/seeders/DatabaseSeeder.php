56ca8e3ee9096e6103c3891f7caf0b631b005981
frayherrera <ufonefray@gmail.com>
2026-08-29 23:09:46 -0500

    fix: remove Hash::make - model casts already hashes password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -17,7 +15,7 @@ class DatabaseSeeder extends Seeder
             ['email' => env('ADMIN_EMAIL', 'admin@admin.co')],
             [
                 'name' => env('ADMIN_NAME', 'Administrador'),
-                'password' => Hash::make(env('ADMIN_PASSWORD', 'password')),
+                'password' => env('ADMIN_PASSWORD', 'password'),
             ]
         );
     }
